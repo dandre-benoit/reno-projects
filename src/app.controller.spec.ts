86a863c899ec2ca114b2150e250ce094ef59f4d4
Benoit D'André <dandre.benoit@outlook.com>
2024-10-01 15:32:13 -0400

    Modified root get endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Renovation Project Management API"', () => {
+      expect(appController.getHome()).toBe('Renovation Project Management API');
     });
   });
 });
